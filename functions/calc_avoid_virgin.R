c2c94e68be9cc57b0d5de214915e173f06c243af
emma-ras <emmarasmussen@ucsb.edu>
2026-07-09 16:10:24 -0700

    hard code for virgin plastic produced

--- functions/calc_avoid_virgin.R
+++ functions/calc_avoid_virgin.R
@@ -14,3 +14,10 @@ consum_bau <- pivot_longer(
 )
 
 write.csv(consum_bau, "data/static/consum_bau.csv", row.names = FALSE)
+
+library(dplyr)
+
+avoid_virign  <- consum_bau %>%
+  left_join(rc_perc_byo, by = c("year", "sector"), suffix = c("_total", "_rc")) %>%
+  mutate(mt_plastic_virgin = mt_plastic_total - mt_plastic_rc) %>%
+  select(year, sector, mt_plastic)
