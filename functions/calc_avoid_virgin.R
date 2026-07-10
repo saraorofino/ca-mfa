0ea581c4836d1c769a4179e77b4109c1e10c430f
emma-ras <emmarasmussen@ucsb.edu>
2026-07-10 12:59:05 -0700

    hard code for avoided virgin production calculations

--- functions/calc_avoid_virgin.R
+++ functions/calc_avoid_virgin.R
@@ -7,29 +7,25 @@
 
 # BAU Consumption stand-in long form, not an input here 
 library(tidyr)
+library(dplyr)
 
-consum_total_bau <- pivot_longer(
-  consum_bau,
-  cols = -year,          # everything except year
-  names_to = "sector",
-  values_to = "mt_plastic"
-)
-
-write.csv(consum_bau, "data/static/consum_bau.csv", row.names = FALSE)
+consum_total_bau <- read.csv("data/static/consum_total_bau.csv") # saved tidy long form
 
 # Filler df for output of consum_total_byo
 
-consum_total_byo_54 <-  pivot_longer(
+consum_total_byo_54 <- read.csv("data/static/consum_total_byo_54.csv") 
+
+consum_total_byo_54_clean <-  pivot_longer(
   consum_total_byo_54,
   cols = -year,          # everything except year
   names_to = "sector",
   values_to = "mt_plastic_byo"
 )
 
-write.csv(consum_total_byo_54, "data/static/consum_total_byo_54.csv", row.names = FALSE)
+write.csv(consum_total_byo_54_clean, "data/static/consum_total_byo_54_clean.csv", row.names = FALSE)
 
 # Filler df for output of rc_perc_byo
-rc_perc_byo_54 <- read.csv("data/rc_perc_byo_54.csv")
+rc_perc_byo_54 <- read.csv("data/static/rc_perc_byo_54.csv")
 
 rc_perc_byo_54_clean <-pivot_longer(
   rc_perc_byo_54,
@@ -38,11 +34,11 @@ names_to = "sector",
 values_to = "mt_plastic_rc"
 )
 
-write.csv(rc_perc_byo_54_clean, "data/static/consum_total_byo_54_clean.csv", row.names = FALSE)
+write.csv(rc_perc_byo_54_clean, "data/static/rc_perc_byo_54_clean.csv", row.names = FALSE)
 
 # calc_avoid_virgin hard code
-avoid_virign  <- consum_bau %>%
-  left_join(rc_perc_byo, by = c("year", "sector"), suffix = c("_total", "_rc")) %>%
+avoid_virign <- consum_total_byo_54_clean %>%
+  left_join(rc_perc_byo_54_clean, by = c("year", "sector")) %>%
   mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
   select(year, sector, mt_plastic_virgin)
 
