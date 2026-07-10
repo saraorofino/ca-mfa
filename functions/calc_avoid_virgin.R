887a0add5d2e77e5f88d875a07a69b1d343fd719
emma-ras <emmarasmussen@ucsb.edu>
2026-07-10 12:31:47 -0700

    add standing clean df

--- functions/calc_avoid_virgin.R
+++ functions/calc_avoid_virgin.R
@@ -1,12 +1,14 @@
 # Calculate avoided virgin plastic production
 # 
 # Inputs: consum_total_byo, rc_perc_byo
+##### Static placeholders delete later: consum_total_byo_54, rc_perc_byo_54
+##### Recycled Content Mandate in SB 54 is 0% so no avoided virgin plastic due to RC 
 # Output: avoid_virgin
 
-# Play around with long form df 
+# BAU Consumption stand-in long form, not an input here 
 library(tidyr)
 
-consum_bau <- pivot_longer(
+consum_total_bau <- pivot_longer(
   consum_bau,
   cols = -year,          # everything except year
   names_to = "sector",
@@ -15,9 +17,33 @@ consum_bau <- pivot_longer(
 
 write.csv(consum_bau, "data/static/consum_bau.csv", row.names = FALSE)
 
-library(dplyr)
+# Filler df for output of consum_total_byo
 
+consum_total_byo_54 <-  pivot_longer(
+  consum_total_byo_54,
+  cols = -year,          # everything except year
+  names_to = "sector",
+  values_to = "mt_plastic_byo"
+)
+
+write.csv(consum_total_byo_54, "data/static/consum_total_byo_54.csv", row.names = FALSE)
+
+# Filler df for output of rc_perc_byo
+rc_perc_byo_54 <- read.csv("data/rc_perc_byo_54.csv")
+
+rc_perc_byo_54_clean <-pivot_longer(
+  rc_perc_byo_54,
+cols = -year,          # everything except year
+names_to = "sector",
+values_to = "mt_plastic_rc"
+)
+
+write.csv(rc_perc_byo_54_clean, "data/static/consum_total_byo_54_clean.csv", row.names = FALSE)
+
+# calc_avoid_virgin hard code
 avoid_virign  <- consum_bau %>%
   left_join(rc_perc_byo, by = c("year", "sector"), suffix = c("_total", "_rc")) %>%
-  mutate(mt_plastic_virgin = mt_plastic_total - mt_plastic_rc) %>%
+  mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
   select(year, sector, mt_plastic_virgin)
+
+# calc_avoid_virgin function 
