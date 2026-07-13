eb79b9d3b1ecdbea19a8eb4621d09881c2b338ca
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-13 11:40:27 -0700

    Adding the hard-coding script from calc_consum_total_byo. Has the output of 'consum_total_byo'

--- functions/calc_consum_total_byo.R
+++ functions/calc_consum_total_byo.R
@@ -6,7 +6,15 @@ library(here)
 consum_bau <- read_csv(here("data","consum-bau.csv"))
 user_inputs_sb54 <- read_csv(here("data","user-inputs-sb54.csv"))
 
-# wide format, need to extract values for user inputs first
+consum_bau_clean <-  pivot_longer(
+  consum_bau,
+  cols = -year,          # everything except year
+  names_to = "sector",
+  values_to = "mt_plastic_bau")
+
+write.csv(consum_bau_clean, "data/consum_bau_clean.csv", row.names = FALSE) #need to alter file path
+
+# extracting needed values from user inputs
 
 target_year_sr <- user_inputs_sb54 |> 
   filter(name == "target_year_sr") |> 
@@ -16,21 +24,34 @@ target_sr <- user_inputs_sb54 |>
   filter(name == "target_sr") |> 
   pull(value)
 
-baseline_year <- user_inputs_sb54 |> 
+baseline_year <-  user_inputs_sb54 |> 
   filter(name == "baseline_year") |> 
   pull(value)
 
+implement_year <-  user_inputs_sb54 |> 
+  filter(name == "implement_year") |> 
+  pull(value)
+
+
+#writing hard code
+
 #creating a reduction multiplier, scaling linearly
-reduction_multiplier <- ifelse(
-  consum_bau$year <= target_year_sr,
-  1- target_sr * (consum_bau$year - baseline_year) / (target_year_sr - baseline_year),
-  1 - target_sr)
+consum_total_byo <- consum_bau_clean |> 
+  #adding a column 'reduction factor' using linear scale based on inputs
+  mutate(reduction_multiplier = ifelse(
+  year <= target_year_sr,
+  (1 - target_sr * 
+      (year - baseline_year) / (target_year_sr - baseline_year)),
+  (1-target_sr))) |>
+  mutate(
+    mt_plastic_byo = case_when(
+      
+      sector == "pack" & year >= implement_year ~ 
+                                    (consum_bau_clean |> 
+                                    filter(year == baseline_year, sector == "pack") |> 
+                                    pull(mt_plastic_bau) * reduction_multiplier),
+  
+      TRUE ~ mt_plastic_bau
+      )) |> 
+  select(!c(mt_plastic_bau, reduction_multiplier))
 
-consum_total_byo <- consum_bau * reduction_multiplier
-
-
-#wide form
-reduction_multiplier <- ifelse(
-  consum_bau$year <= user_inputs_sb54$target_year_sr,
-        1- user_inputs_sb54$target_sr * (consum_bau$year - user_inputs_sb54$baseline_year) / (user_inputs_sb54$target_year - user_inputs_sb54$start_year),
-                               1 - user_inputs_sb54$target_sr)
