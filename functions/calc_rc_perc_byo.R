a2be684cda18e5a62bf7fbc519296a6e8adc0d0b
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-13 16:56:37 -0700

    pushing hardcode for calculating the "Post-consumer recycled content in CA plastic consumption due to PCR intervention" in the calc_rc_perc_byo function.

---
+++ functions/calc_rc_perc_byo.R
@@ -0,0 +1,50 @@
+
+library(tidyverse)
+library(here)
+
+consum_total_byo <- read_csv(here("data","consum_total_byo.csv"))
+user_inputs_sb54 <- read_csv(here("data", "user-inputs-sb54.csv"))
+
+
+# pulling values from user inputs -----------------------------------------
+
+target_rc <- 0.4
+  
+  user_inputs_sb54 |> 
+  filter(name == "target_rc") |> 
+  pull(value)  |> 
+  as.numeric()
+
+target_year_rc <- user_inputs_sb54 |> 
+  filter(name == "target_year_rc") |> 
+  pull(value)  |> 
+  as.numeric()
+
+baseline_year <- user_inputs_sb54 |> 
+  filter(name == "baseline_year") |> 
+  pull(value) |> 
+  as.numeric()
+
+implement_year <- user_inputs_sb54 |> 
+  filter(name == "implement_year") |> 
+  pull(value) |> 
+  as.numeric()
+
+target_sector <- user_inputs_sb54 |> 
+  filter(name == "target_sector") |> 
+  pull(value)
+
+baseline_rc <- 0 #should we add this to inputs
+
+
+# calculating recycled content rate
+
+consum_total_byo_test <- consum_total_byo |> 
+  filter(!c(sector == "total")) |>  #should we have another dataframe with just totals across all sectors?
+  mutate( rc_rate = 
+          case_when(
+            year >= implement_year & sector == 'pack' & year <= target_year_rc ~
+                     baseline_rc + (target_rc - baseline_rc) * (year - baseline_year) / (target_year_rc - baseline_year), 
+          year >= implement_year & sector == 'pack' & year >= target_year_rc ~ target_rc,  
+            
+          TRUE ~ 0))
