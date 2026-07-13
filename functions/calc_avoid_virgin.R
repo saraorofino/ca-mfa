aea7e2406e73b2bdf132bb30559e10b61f9a91f0
emma-ras <emmarasmussen@ucsb.edu>
2026-07-13 12:53:01 -0700

    added documentation to function. adding in summary output

--- functions/calc_avoid_virgin.R
+++ functions/calc_avoid_virgin.R
@@ -1,21 +1,23 @@
-# Calculate avoided virgin plastic production
-# 
-# Inputs: consum_total_byo, rc_perc_byo
-##### Static placeholders delete later: consum_total_byo_54, rc_perc_byo_54
-##### Recycled Content Mandate in SB 54 is 0% so no avoided virgin plastic due to RC 
-# Output: avoid_virgin
+#' @title Calculate avoided virgin plastic production
+#'
+#' @param consum_total_byo, rc_perc_byo
+#' Static placeholders delete later: consum_total_byo_54, rc_perc_byo_54 Recycled Content Mandate in SB 54 is 0% so no avoided virgin plastic due to RC 
+#' @output avoid_virgin
 
-# BAU Consumption stand-in long form, not an input here 
+# load data ---------------------------------------------------------------
+# Delete in function version 
 library(tidyr)
 library(dplyr)
 
 consum_total_bau <- read.csv("data/static/consum_total_bau.csv") # saved tidy long form
 
-# Filler df for output of consum_total_byo
+consum_total_byo_54 <- read.csv("data/static/consum_total_byo_54.csv") # convert to long form 
 
-consum_total_byo_54 <- read.csv("data/static/consum_total_byo_54.csv") 
+rc_perc_byo_54 <- read.csv("data/static/rc_perc_byo_54.csv")
 
-consum_total_byo_54_clean <-  pivot_longer(
+# clean filler df --------------------------------------------------------
+# consum_byo clean long format filler data frame clean 
+consum_total_byo_54_clean <-  pivot_longer( # change to 
   consum_total_byo_54,
   cols = -year,          # everything except year
   names_to = "sector",
@@ -24,8 +26,7 @@ consum_total_byo_54_clean <-  pivot_longer(
 
 write.csv(consum_total_byo_54_clean, "data/static/consum_total_byo_54_clean.csv", row.names = FALSE)
 
-# Filler df for output of rc_perc_byo
-rc_perc_byo_54 <- read.csv("data/static/rc_perc_byo_54.csv")
+# rc_perc_byo clean long format filler data frame for output
 
 rc_perc_byo_54_clean <-pivot_longer(
   rc_perc_byo_54,
@@ -36,10 +37,14 @@ values_to = "mt_plastic_rc"
 
 write.csv(rc_perc_byo_54_clean, "data/static/rc_perc_byo_54_clean.csv", row.names = FALSE)
 
-# calc_avoid_virgin hard code
-avoid_virgin <- consum_total_byo_54_clean %>%
-  left_join(rc_perc_byo_54_clean, by = c("year", "sector")) %>%
-  mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
-  select(year, sector, mt_plastic_virgin)
+# hardcode calc avoid virgin ----------------------------------------------
 
-# calc_avoid_virgin function 
+#avoid_virgin <- consum_total_byo_54_clean %>%
+ # left_join(rc_perc_byo_54_clean, by = c("year", "sector")) %>%
+ # mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
+ # select(year, sector, mt_plastic_virgin)
+
+
+# test function  ----------------------------------------------------------
+
+avoid_virgin <- avoid_virgin_function(consum_total_byo_54_clean, rc_perc_byo_54_clean) 
--- functions/calc_avoid_virgin_function.R
+++ functions/calc_avoid_virgin_function.R
@@ -1,6 +1,26 @@
+#' @title Avoided virgin plastic production
+#' @param consum_total_byo data frame output of consumption after source reduction policy
+#' @param rc_perc_byo data frame output of consumption after
+#' @param summary Logical. If 'FALSE' (default), returns detailed data frame by year and sector. If 'TRUE', returns a summary data frame with total mt_plastic_avoided across all years and the amount avoided in-state vs. out-of-state. 
+#' @description
+#' This function calculates avoided plastic production based on the consumption levels after source reduction. It subtracts the sector's per year recycled content amount from the consumption levels 
+#' @return A data frame with columns for year, sector, and metric tons of avoided virgin plastic called mt_plastic_virgin
+#'
+avoid_virgin_function <- function(consum_total_byo, rc_perc_byo, summary = FALSE) {
+  detailed <- consum_total_byo %>%
+    left_join(rc_perc_byo, by = c("year", "sector")) %>%
+    mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
+    select(year, sector, mt_plastic_virgin)
+  if(!summary) {
+    return(detailed)
+  }
+ total <- sum(detailed$mt_plastic_avoided)
+   #####add in summary 
+}
 
+# Works 
 avoid_virgin_function <- function(consum_total_byo, rc_perc_byo) {
-  consum_total_byo_54_clean %>%
+  consum_total_byo %>%
     left_join(rc_perc_byo, by = c("year", "sector")) %>%
     mutate(mt_plastic_virgin = mt_plastic_byo - mt_plastic_rc) %>%
     select(year, sector, mt_plastic_virgin)
