ad806707cd53e7a1ffda9eb65ee0591ccb6df01e
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-13 13:35:54 -0700

    Adding section labels. Adding caclculations for the total consumed across all sectors per year BYO. Writing csv for final output (of this function)

--- functions/calc_consum_total_byo.R
+++ functions/calc_consum_total_byo.R
@@ -1,10 +1,17 @@
-# hard coding before I convert to function
+
+# reading in data and packages --------------------------------------------
+
+
 
 library(tidyverse)
 library(here)
 
 consum_bau <- read_csv(here("data","consum-bau.csv"))
 user_inputs_sb54 <- read_csv(here("data","user-inputs-sb54.csv"))
+
+
+# cleaning cansum_bau_clean -----------------------------------------------
+
 
 consum_bau_clean <-  pivot_longer(
   consum_bau,
@@ -14,7 +21,9 @@ consum_bau_clean <-  pivot_longer(
 
 write.csv(consum_bau_clean, "data/consum_bau_clean.csv", row.names = FALSE) #need to alter file path
 
-# extracting needed values from user inputs
+
+# extracting values from user inputs --------------------------------------
+
 
 target_year_sr <- user_inputs_sb54 |> 
   filter(name == "target_year_sr") |> 
@@ -33,9 +42,10 @@ implement_year <-  user_inputs_sb54 |>
   pull(value)
 
 
-#writing hard code
 
-#creating a reduction multiplier, scaling linearly
+# hardcode ----------------------------------------------------------------
+
+
 consum_total_byo <- consum_bau_clean |> 
   #adding a column 'reduction factor' using linear scale based on inputs
   mutate(reduction_multiplier = ifelse(
@@ -45,7 +55,7 @@ consum_total_byo <- consum_bau_clean |>
   (1-target_sr))) |>
   mutate(
     mt_plastic_byo = case_when(
-      
+      # if the year is above the implementation year, multiply BAU consumption from the implementation year by reduction multiplier
       sector == "pack" & year >= implement_year ~ 
                                     (consum_bau_clean |> 
                                     filter(year == baseline_year, sector == "pack") |> 
@@ -55,3 +65,22 @@ consum_total_byo <- consum_bau_clean |>
       )) |> 
   select(!c(mt_plastic_bau, reduction_multiplier))
 
+
+# calculating totals ------------------------------------------------------
+
+totals <- consum_total_byo |>  #calculating the totals per year to later bind with full dataframe
+  group_by(year) |> 
+  summarize(
+    sector = "total",
+    mt_plastic_byo = sum(mt_plastic_byo),
+    .groups = "drop")
+
+
+# adding totals to df -----------------------------------------------------
+
+consum_total_byo <- bind_rows(consum_total_byo, totals) |> 
+  arrange(desc(year), sector == "total", sector)
+
+write.csv(consum_total_byo, "data/consum_total_byo.csv", row.names = FALSE)
+
+  
