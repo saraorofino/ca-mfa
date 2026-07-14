77f34827341bab99a9ffe4afd1425eecf8628314
emma-ras <emmarasmussen@ucsb.edu>
2026-07-14 15:43:03 -0700

    add calc scrap function code

--- functions/calc_scrap_input_function.R
+++ functions/calc_scrap_input_function.R
@@ -6,3 +6,22 @@
 #' @description
 #' This function calculates scrap input to the state using the post consumer recycled content production target inputs to work backwards with the recycling yield loss to find the total amount of recycled plastic input. 
 #' @return A data frame 'scrap_input" with columns for year, sector, and megatonnes of scrap input. 
+
+calc_scrap_input_function <- function(rc_perc_byo, recyc_yield, ca_scrap_consump, summary = FALSE) {
+  detailed <- rc_perc_byo  %>%
+    mutate(scrap_input = mt_plastic_rc / recyc_yield)%>%
+    select(year, sector, scrap_input) 
+  if(!summary) {
+    return(detailed)
+  }
+  summarized <- detailed %>%
+    filter(sector != "all_sec") %>% # removes all sector totals per year
+    summarise(total_scrap = sum(scrap_input)) %>%
+    mutate(scrap_is = (total_scrap * ca_scrap_consump),
+           scrap_oos = total_scrap * (1 - ca_scrap_consump))
+}  
+  
+  
+  
+  
+ 
