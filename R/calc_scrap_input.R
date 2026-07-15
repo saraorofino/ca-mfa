6b00484d8a57c0dbb6074318b3ef63fa04557669
emma-ras <emmarasmussen@ucsb.edu>
2026-07-15 09:19:12 -0700

    consistent between code and function description

--- R/calc_scrap_input.R
+++ R/calc_scrap_input.R
@@ -1,11 +1,12 @@
-#' @title Plastic Scrap Input to State
-#' @param rc_perc_byo data frame output of consumption after recycled content policy
+#' @title Secondary Plastic Input for Recycled Content Targets
+#' @param rc_perc_byo Data frame output of secondary plastic consumption from the recycled content policy.
 #' @param recyc_yield Material loss rate from gathering recycled plastic to finished recycled content product. 
-#' @param ca_scrap_consump In-state percent of scrap consumption  
-#' @param summary Logical. If 'FALSE' (default), returns detailed data frame by year and sector. If 'TRUE', returns a summary data frame with total scrap across 1950 to 2050. 
+#' @param ca_scrap_consump In-state percent of scrap consumption.  
+#' @param summary Logical. If 'FALSE' (default), returns a detailed data frame. If 'TRUE', returns a summary data frame with cumulative scrap from 1950 to 2050 across all sectors. 
 #' @description
-#' This code calculates scrap input to the state using the post consumer recycled content production target inputs and the recycling yield loss to work backwards to find the total amount of recycled plastic input. 
-#' @return A detailed data frame 'scrap_input' with columns for year, sector, and megatonnes of scrap input if summary is FALSE and a data frame 'summarized' if summary is TRUE with 1950-2050 cumulative, in-state and out-of-state scrap input in megatonnes. 
+#' This code calculates scrap input to the state using the post consumer recycled content production target consumption to work backwards with the recycling yield loss to find the total amount of recycled plastic input. 
+#' @return If summary 'FALSE' a data frame 'scrap_input" with columns for year, sector, and megatons of scrap input. 
+#' @return If summary 'TRUE', a data frame 'summarized' with 1950-2050 cumulative, in-state and out-of-state scrap input in megatons. 
 
 
 # upload data -------------------------------------------------------------
