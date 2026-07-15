b3b0a1fccbfa2a853d66fa912dd72043ea1aed12
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-15 10:02:24 -0700

    Updated parameters in accordance with google doc. renamed consum_bau_clean parameter to consum_bau. Renamed output of function to consum_byo from consum_total_byo. Added the 'all_sec' row description to return.

--- functions/calc_consum_total_byo_function.R
+++ functions/calc_consum_total_byo_function.R
@@ -1,16 +1,15 @@
 #' @title Total Consumption under the Build Your Own scenario function.
-#' @param consum_bau_clean Cleaned dataframe of business as usual consumption
+#' @param consum_bau Data frame output of consumption under the business as usual (BAU) scenario.
 #' @param target_year_sr The target year for the source reduction to take full effect 
 #' @param target_sr The target source reduction, as a proportion.
 #' @param baseline_year The baseline year of consumption under business as usual. Used to pull consumption value for source reduction.
 #' @param target_sector The sector which will be the target of the source reduction policy.
 #' @param implement_year The year of implementation of the source reduction policy, where reduction multipliers will be used.
-#' @description
-#' This function calculates the plastic consumption under source reduction in the build your own scenario (BYO). Based upon the business as usual consumption, and policy inputs.
-#' @return A data frame 'consum_total_byo" with columns for year, sector, and megatonnes of plastic .
+#' @description This function calculates the plastic consumption under source reduction in the build your own scenario (BYO). Uses linear scaling to forecast source reduction rates based on the target source reduction policy input, and multiplies by the consumption under the business as usual scenario.
+#' @return A data frame 'consum_byo" with columns for year, sector, and mega tones of plastic (mt_plastic_byo). Also contains an “all_sec” row each year with the total consumption across all sectors for that year.
 
 
-calc_byo_consum_function <- function(consum_bau_clean, 
+calc_byo_consum_function <- function(consum_bau, 
                                       target_year_sr, 
                                       target_sr, 
                                       baseline_year,
@@ -28,7 +27,7 @@ calc_byo_consum_function <- function(consum_bau_clean,
 
 # calculating the reduction multiplier ------------------------------------
    
-  consum_total_byo <- consum_bau_clean |> 
+  consum_byo <- consum_bau |> 
     mutate(
       reduction_multiplier = ifelse(
         year <= target_year_sr,
@@ -39,7 +38,7 @@ calc_byo_consum_function <- function(consum_bau_clean,
 
 # calculating per sector consumption -----------------------------------------------
   
-  consum_total_byo <- consum_total_byo |>  
+  consum_byo <- consum_byo |>  
     mutate(
       mt_plastic_byo = case_when(
         sector == target_sector & year >= implement_year ~ baseline_sec_value * reduction_multiplier,
@@ -51,17 +50,17 @@ calc_byo_consum_function <- function(consum_bau_clean,
 
 # calculating yearly totals across all sectors ----------------------------
 
-  all_sec <- consum_total_byo |>  #calculating the totals per year to later bind with full dataframe
+  all_sec <- consum_byo |>  #calculating the totals per year to later bind with full dataframe
     group_by(year) |> 
     summarize(
       sector = "all_sec",
       mt_plastic_byo = sum(mt_plastic_byo),
       .groups = "drop")
   
-  consum_total_byo <- bind_rows(consum_total_byo, all_sec) |> 
+  consum_byo <- bind_rows(consum_total_byo, all_sec) |> 
     arrange(desc(year), sector == "all_sec", sector)
 
-consum_total_byo
+
 
 }
 
