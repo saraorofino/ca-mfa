7bbbd27fb0906a0a86d4bf3163b875f964b99cd5
emma-ras <emmarasmussen@ucsb.edu>
2026-07-16 17:30:28 -0700

    add in code for collected recycling

--- R/test_calc_collect_recyc_bau.R
+++ R/test_calc_collect_recyc_bau.R
@@ -9,11 +9,72 @@ library(tidyverse)
 library(here)
 library(dplyr)
 
+bau_rr <- read_csv(here("data","static","bau_rr.csv")) #copy to preprocessing 
+wastegen_bau <- read_csv(here("data","static","wastegen_bau.csv"))
+wastegen <- read_csv(here("data","static","wastegen_54.csv"))
 
-bau_rr <- read_csv(here("data","static","bau_rr.csv"))
-wastegen <- read_csv(here("data","static",".csv"))
+# preprocessing bau -------------------------------------------------------
+# clean data 
+wastegen_bau <- pivot_longer(
+  wastegen_bau,
+  cols = -year,          # everything except year
+  names_to = "sector",
+  values_to = "mt_plastic_wastegen"
+) 
+
+wastegen <- pivot_longer(
+  wastegen,
+  cols = -year,          # everything except year
+  names_to = "sector",
+  values_to = "mt_plastic_wastegen"
+) 
+
+# hard code draft ---------------------------------------------------------------
+implement_year_rr <- 2025
+target_rr <- 0.65
+target_year_rr <- 2032
+
+baseline_rate <- bau_rr |> 
+  filter(year == (implement_year_rr -1)) |> 
+  pull(bau_rr) 
+
+wastegen <- wastegen |> 
+  filter(sector== 'pack')|>
+  left_join(bau_rr, by = "year") 
+  
+
+calc_collect_recyc <- wastegen |>
+  mutate(
+    baseline_year = implement_year_rr - 1,
+    rr_multiplier = case_when(
+      target_rr <= baseline_rate ~ bau_rr,
+      year <= baseline_year ~ baseline_rate,
+      year > baseline_year & year <= target_year_rr ~
+        baseline_rate + (target_rr - baseline_rate) * (year - baseline_year) / (target_year_rr - baseline_year),
+      year > target_year_rr ~ target_rr,
+      TRUE ~ bau_rr
+    )
+  ) # add for year < implement year 
+
+# calculating per year collection -----------------------------------------------
+
+mutate(
+  mt_plastic_collec = case_when(
+    sector == packaging & year > implement_year ~ baseline_rate * rr_multiplier,
+    TRUE ~ wastegen * rr_bau
+  )
+) |> 
+  select( -rr_multiplier)
 
 
-# hard code ---------------------------------------------------------------
+
+# test function -----------------------------------------------------------
+
+#sector == 'target_sector' can only be packaging
+#calc_collect_recyc(wastegen) only have wastegen bau will be done in prepreprocessing
+
+
+  
+
 
 
---
+++ functions/calc_collect_recyc.R
@@ -0,0 +1,8 @@
+#' @title Collected Recycling Business-as-Usual 
+#' @param wastegen Data frame output of the waste generated based on consumption and disposal lifetimes.
+#' @param bau_rr Recycling rate for the packaging sector per year based on CalRecycle data.
+#' @reference PLACEHOLDER FOR CAL RECYCLE INFO
+#' @description
+#' PLACEHOLDER Description
+#' 
+#' 
