c7696fdd332b34aefe5f547e41aaaf2b3f49375f
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-17 12:39:26 -0700

    Updated the calc_consum_sr functions to include baseline year, changed paramets to all have _sr suffix.

--- functions/calc_consum_sr.R
+++ functions/calc_consum_sr.R
@@ -2,49 +2,51 @@
 #' @param consum_bau Data frame output of consumption under the business as usual (BAU) scenario.
 #' @param target_year_sr The target year for the source reduction to take full effect 
 #' @param target_sr The target source reduction, as a proportion.
-#' @param target_sector The sector which will be the target of the source reduction policy.
-#' @param implement_year The year of implementation of the source reduction policy. Reduction multipliers will be used in the following year, and baseline consumptions will be pulled from the year prior.
+#' @param target_sector_sr The sector which will be the target of the source reduction policy.
+#' @param baseline_year_sr The baseline year of consumption under business as usual. Used to pull consumption value for source reduction
+#' @param implement_year_srThe year of implementation of the source reduction policy. Reduction multipliers will be used in the following year, and baseline consumptions will be pulled from the year prior.
 #' @description This function calculates the plastic consumption under source reduction in the build your own scenario (BYO). Uses linear scaling to forecast source reduction rates based on the target source reduction policy input, and multiplies by the consumption under the business as usual scenario.
 #' @return A data frame 'consum_sr" with columns for year, sector, and mega tones of plastic (mt_plastic_byo). Also contains an “all_sec” row each year with the total consumption across all sectors for that year.
 
 
 calc_consum_sr <- function(consum_bau, 
-                                      target_year_sr, 
-                                      target_sr, 
-                                      target_sector,
-                                      implement_year) {
-
-# calculating the baseline sector value -----------------------------------
-   
+                           target_year_sr, 
+                           target_sr, 
+                           target_sector_sr,
+                           baseline_year_sr,
+                           implement_year_sr) {
+  
+  # calculating the baseline sector value -----------------------------------
+  
   baseline_sec_value <- consum_bau |> 
-    filter(year == (implement_year -1), sector == target_sector) |> 
+    filter(year == (baseline_year_sr), sector == target_sector_sr) |> 
     pull(mt_plastic_bau) 
   
-
-# calculating the reduction multiplier ------------------------------------
-   
+  
+  # calculating the reduction multiplier ------------------------------------
+  
   consum_sr <- consum_bau |> 
     mutate(
       reduction_multiplier = ifelse(
         year <= target_year_sr,
-        (1 - target_sr * (year - implement_year) / (target_year_sr - implement_year)), 
+        (1 - target_sr * (year - implement_year_sr) / (target_year_sr - implement_year_sr)), 
         (1 - target_sr)
       )
     ) |> 
-
-# calculating per sector consumption -----------------------------------------------
+    
+    # calculating per sector consumption -----------------------------------------------
   
-    mutate(
-      mt_plastic_byo = case_when(
-        sector == target_sector & year > implement_year ~ baseline_sec_value * reduction_multiplier,
-        TRUE ~ mt_plastic_bau
-      )
-    ) |> 
+  mutate(
+    mt_plastic_byo = case_when(
+      sector == target_sector_sr & year > implement_year_sr~ baseline_sec_value * reduction_multiplier,
+      TRUE ~ mt_plastic_bau
+    )
+  ) |> 
     select(-mt_plastic_bau, -reduction_multiplier)
   
-
-# calculating yearly totals across all sectors ----------------------------
-
+  
+  # calculating yearly totals across all sectors ----------------------------
+  
   all_sec <- consum_sr |>  #calculating the totals per year to later bind with full dataframe
     group_by(year) |> 
     summarize(
@@ -54,9 +56,9 @@ calc_consum_sr <- function(consum_bau,
   
   consum_sr <- bind_rows(consum_sr, all_sec) |> 
     arrange(desc(year), sector == "all_sec", sector)
-
-return(consum_sr)
-
+  
+  return(consum_sr)
+  
 }
 
 
