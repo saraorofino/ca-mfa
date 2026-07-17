df0bd0e50a83e07ad136e1fc1239510cc25af077
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-17 16:20:34 -0700

    Created a hard-coded loop in order to calculated the waste generated across all years and all sectors 1950-2050. Will convert to a function.

--- tests/test_calc_wastegen.R
+++ tests/test_calc_wastegen.R
@@ -56,7 +56,53 @@ waste_gen_looped <- data.frame(
                       sector = character(),
                       mt_plastic_wastegen = numeric()) 
 
+sectors <- unique(consum_bau_clean$sector)
+years <- sort(unique(consum_bau_clean$year))
 
+for (current_sector in sectors) {
+  
+  # part 1: pull the lifetime values for each of the 11 sectors
+  
+  lifetime_values <- lifetimes_clean |> #should i rename this to lifetime_dist?
+    filter(
+      sector == target_sector,
+      year <= 70 #only uses first 70 years as per the excel model
+    ) |>
+    arrange(year) |>
+    pull(lifetime)
+  
+  # part 2: pull out the consumption values for the 70 years leading up to the current year
+  
+  for (current_year in years) {
+    
+    consum_values <-  consum_bau_clean |> 
+    
+      filter(
+      sector == current_sector,
+      year < current_year, #ensuring it is the year prior, does not include current year
+      year >= current_year - 70 ) |>
+      arrange(desc(year)) |>
+      pull(mt_plastic_bau)
+    
+      # ensures there are 70 years of consumption data by replacing missing values with 0 (as done in excel)
+      consum_values <- c(
+        consum_values,
+        rep(0, 70 - length(consum_values))
+      )
+      
+  # part 3: sums the product of the lifetime and the consumption for the current year
+  
+      wastegen_current_year <- sum(
+        consum_values * lifetime_values) # translated sumproduct into r, multiplies the 70 years of consumption and lifetimes prior to the current year and sums them for the wastegen value
+    
+      waste_gen_looped <- bind_rows(
+        waste_gen_looped,
+        data.frame(
+          year = current_year,
+          sector = current_sector,
+          mt_plastic_wastegen = wastegen_current_year)) |> 
+        arrange(desc(year))
+      }}
 
 
 
