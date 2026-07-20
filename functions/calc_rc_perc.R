6d3b028c438b1a79d7a4070018dd65d8e70ef649
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-20 16:29:55 -0700

    Created consum_analysis script in new analysis folder
    also edited calc_rc_perc to change all _byo suffixes to _sr.

--- functions/calc_rc_perc.R
+++ functions/calc_rc_perc.R
@@ -27,8 +27,8 @@ calc_rc_perc <- function(consum, target_rc, target_year_rc, implement_year, targ
 # calculating recycled content weight -------------------------------------
 
   rc_perc <- rc_perc |> 
-    mutate(mt_plastic_rc = mt_plastic_byo * rc_rate) |> 
-    select(!c(mt_plastic_byo)) 
+    mutate(mt_plastic_rc = mt_plastic_sr * rc_rate) |> 
+    select(!c(mt_plastic_sr)) 
   
   return(rc_perc)
 
