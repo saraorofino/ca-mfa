b33b2881ae71bf496a7f50aa58df39cff7d162d8
matthewroco-calvo <matthewf.rc@gmail.com>
2026-07-20 13:53:17 -0700

    added hardcode for calc_avoid_wastegen

--- tests/test_calc_avoid_wastegen.R
+++ tests/test_calc_avoid_wastegen.R
@@ -1,3 +1,19 @@
 
-avoid_wastegen <- 
-  (wastegen_bau$mt_wastegen - wastegen_byo$mt_wastegen)
+wastegen_byo_clean <- wastegen_byo |> 
+  filter(sector != "all_sec") |> 
+  arrange(desc(year), sector)
+
+wastegen_bau_clean <- wastegen_bau |> 
+  arrange(desc(year), sector)
+
+
+avoid_wastegen <- wastegen_bau_clean |> 
+  inner_join(
+    wastegen_byo_clean,
+    by = c("year", "sector"),
+    suffix = c("_bau", "_byo")) |> 
+
+  mutate(mt_avoid_wastegen = (mt_plastic_wastegen_bau - mt_plastic_wastegen_byo)) |> 
+  select("year", "sector", "mt_avoid_wastegen")
+
+sum(avoid_wastegen$mt_avoid_wastegen)
