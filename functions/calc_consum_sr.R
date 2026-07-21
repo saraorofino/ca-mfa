d3893f27018d2e5d1935f37ab74b6c70fab0214a
emma-ras <emmarasmussen@ucsb.edu>
2026-07-21 15:25:29 -0700

    Edited small typos

--- functions/calc_consum_sr.R
+++ functions/calc_consum_sr.R
@@ -1,10 +1,10 @@
 #' @title Total Consumption under the source reduction function.
 #' @param consum_bau Data frame output of consumption under the business as usual (BAU) scenario.
-#' @param target_year_sr The target year for the source reduction to take full effect 
+#' @param target_year_sr The target year for the source reduction to take full effect. 
 #' @param target_sr The target source reduction, as a proportion.
 #' @param target_sector_sr The sector which will be the target of the source reduction policy.
 #' @param baseline_year_sr The baseline year of consumption under business as usual. Used to pull consumption value for source reduction
-#' @param implement_year_srThe year of implementation of the source reduction policy. Reduction multipliers will be used in the following year, and baseline consumptions will be pulled from the year prior.
+#' @param implement_year_sr The year of implementation of the source reduction policy. Reduction multipliers will be used in the following year, and baseline consumption will be pulled from the year prior.
 #' @description This function calculates the plastic consumption under source reduction in the build your own scenario (BYO). Uses linear scaling to forecast source reduction rates based on the target source reduction policy input, and multiplies by the consumption under the business as usual scenario.
 #' @return A data frame 'consum_sr" with columns for year, sector, and mega tones of plastic (mt_plastic_sr). Also contains an “all_sec” row each year with the total consumption across all sectors for that year.
 
