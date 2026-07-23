383b8732fed89d6a5bde071cdea48d621c7c45ab
emma-ras <emmarasmussen@ucsb.edu>
2026-07-23 11:22:01 -0700

    #31 updated final data frame output

--- functions/calc_eol.R
+++ functions/calc_eol.R
@@ -12,7 +12,7 @@ calc_eol <- function(wastegen, recyc_output, incineration)
     left_join(recyc_output, by = c("year", "sector")) |>
     left_join(incineration, by = "year") |>
     mutate(mt_plastic_landfill = mt_plastic_wastegen - mt_secondary_plastic_output - mt_incin) |>
-    select(year, sector, mt_plastic_landfill)
-  return(landfill)
+    select(year, sector, mt_plastic_landfill, mt_secondary_plastic_output, mt_incin)
+  return(eol)
 }
 
