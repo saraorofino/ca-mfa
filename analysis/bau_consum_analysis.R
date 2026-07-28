b1105e7baa3085c0dbe097652012f267615568de
emma-ras <emmarasmussen@ucsb.edu>
2026-07-28 16:16:09 -0700

    Added functions for pre processing bau

--- analysis/bau_consum_analysis.R
+++ analysis/bau_consum_analysis.R
@@ -11,7 +11,16 @@ rho_data <- get_rho_data()
 
 
 # Get Matrix Data -------------------------------------------------------
-matrix_data <- get_matrix_data()
+matrix_data_L <- get_matrix_data(tab_name = "L")
+
+matrix_data_A <- get_matrix_data(tab_name = "A")
+
+complete_consumption <- get_complete_consum() 
+
+
+# Calculate Plastic Intensity ---------------------------------------------
+
+
 
 # plastic consumption io --------------------------------------------------
 io_file <- file.path(here::here("data/raw/CAEEIO_326_output_2012_2020_v4.xlsx"))
---
+++ functions/calc_deflated_plastic_intensity.R
@@ -0,0 +1,9 @@
+#' @description
+#' Plastic intensity is the total mass output of US plastic tons divided by the economic value of output $ 
+#' 
+
+calc_deflated_plastic_intensity <- function(rho, complete_consumption)
+
+ consum_2020_dollars <-sum(complete_consumption$us_consumption_complete)
+                                            
+                                
