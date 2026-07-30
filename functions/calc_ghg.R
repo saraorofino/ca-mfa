8432b87fc486d90eec02ec5686526aa91dab8da6
emma-ras <emmarasmussen@ucsb.edu>
2026-07-30 10:39:06 -0700

    Fixed error in calc_ghg. Data frame had space in incineration, new clean df in share folder

--- functions/calc_ghg.R
+++ functions/calc_ghg.R
@@ -5,7 +5,7 @@
 #' @param target_sector: The target sector for avoid_prim_prod.
 #' @param implement_year: The year of implementation.
 #' @references Pottinger, A. Samuel, et al. “Pathways to Reduce Global Plastic Waste Mismanagement and Greenhouse Gas Emissions by 2050.” Science, vol. 0, no. 0, Nov. 2024, p. eadr3837. science.org (Atypon), https://doi.org/10.1126/science.adr3837.
-#' @description Calculates the Mt of CO2 equivalent emitted from production (consumption), end of life (reycling, landfill, and incineration), and the avoided primary production. 
+#' @description Calculates the Mt of CO2 equivalent emitted from production (consumption), end of life (recycling, landfill, and incineration), and the avoided primary production. 
 #' @return Returns 3 dataframes, greenhouse gasses in MtCO2 equivalent from production, end of life, and avoided primary production, (ghg_prod, ghg_eol, ghg_avoid_prim_prod respectively).
 
 
@@ -43,29 +43,27 @@ calc_ghg <- function(consum,
   
 
 # part 2: calculating ghg_eol -----------------------------------
-
-
-  emission_factors_eol <- emission_factors |> 
-    filter(type == 'eol')
   
-  landfill_ef <- emission_factors_eol |> 
+  landfill_ef <- emission_factors |> #remove_eol
     filter(sector == 'landfill') |> 
     pull(emission_factor)
-  incineration_ef <- emission_factors_eol |> 
+  
+  incineration_ef <- emission_factors |> 
     filter(sector == 'incineration') |> 
     pull(emission_factor)
-  recyc_ef <- emission_factors_eol |> 
+  
+  recyc_ef <- emission_factors |> 
     filter(sector == 'recycling') |> 
     pull(emission_factor)
   
   ghg_eol <- eol |> 
     #calculating co2e for each disposal type per year
     mutate(mt_co2e_landfill = mt_plastic_landfill * landfill_ef ) |> 
-    mutate(mt_co2e_incineration = mt_incin * incineration_ef ) |>  #changed incin_mt to mt_incin
+    mutate(mt_co2e_incineration = mt_incin * incineration_ef) |>  #changed incin_mt to mt_incin
     mutate(mt_co2e_recyc = mt_secondary_plastic_output * recyc_ef ) |> 
     #summing all 3 disposal types together
     mutate(mt_co2e_eol = mt_co2e_landfill + mt_co2e_incineration + mt_co2e_recyc) |> 
-    filter(year >= implement_year)
+    filter(year >= implement_year) 
   
   # part 3: calculating ghg_avoid_prim_prod --------------------------------------------------
   
@@ -76,8 +74,7 @@ calc_ghg <- function(consum,
   
   ghg_avoid_prim_prod <- eol |> 
     mutate(mt_co2e_avoidprod = -mt_secondary_plastic_output * 0.8 * avoid_prim_prod_ef) |> 
-    filter(year >= implement_year)
-
+    filter(year >= implement_year) 
 
   return(list(
     ghg_prod = ghg_prod,
