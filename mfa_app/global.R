a58dbfbd5331afca1a1a58a6feb38e06d871ed20
matthewroco-calvo <matthewf.rc@gmail.com>
2026-08-13 14:04:17 -0700

    changed ca_rr to ca_rr_pack and changed associated file to read in, aligns with spreadsheet.

--- mfa_app/global.R
+++ mfa_app/global.R
@@ -17,7 +17,7 @@ library(rsconnect) # delete?
 # Load data --------------------------------------------------------
 
 lifetimes <- read.csv(here::here("data","static","lifetimes_clean.csv"))
-ca_rr <- read.csv(here::here("data", "static", "ca_rr.csv")) 
+ca_rr <- read.csv(here::here("data", "static", "ca_rr_pack.csv")) 
 ca_incineration <- read.csv(here::here("data", "static", "incineration_clean.csv")) 
 emission_factors <- read.csv(here('data', 'static', 'emission_factors_clean.csv'))
 
@@ -29,4 +29,9 @@ consum_bau <- read.csv(here::here("data","static","consum_bau.csv")) # DELETE IN
 ###### OR put all functions in R/ folder for shiny 
 list.files(here::here("functions"), full.names = TRUE) |>
   purrr::walk(source)
+
+#loading run_policy functions (or should we run all function in "R" folder)
+
+source("R/run_policy_sr.R") 
+
 
