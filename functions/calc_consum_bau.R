64f0dae63988b86f825b41c76927e9fae1dafb06
saraorofino <saraorofino@bren.ucsb.edu>
2026-08-17 19:48:31 -0700

    for nested functions the outer function should explicitly have inputs that are required by the inner functions (#46)

--- functions/calc_consum_bau.R
+++ functions/calc_consum_bau.R
@@ -62,64 +62,72 @@ available_states <- sort(unique(sub(paste0(".*", file_pattern), "\\1", rds_files
 
 
 # Pull all functions into environment -------------------------------------
-
+## This + static files should be moved all into one script
 list.files(here::here("functions"), full.names = TRUE) |>
   purrr::walk(source)
 
 
-# 00 Sector percentages pre-processing ---------------------------------------
 
-props <- preprocess_sectors() ### this output seems to be missing some bea sectors??
-
-calc_consum_bau <- function() {
-
-# 01 Create State Long Data ---------------------------------------------
-
-download_rds_state_model(state_abbr = "CA") # Reactive input in shiny 
-
-# 02 Calculate deflated/inflated plastic intensity (m) ------------------------
-
-m <- calc_deflated_plastic_int("CA") 
+bea_to_plastic <- read_xlsx(path = file.path(here::here("data/raw/plastic_sector_classification.xlsx")),
+                            sheet = "plastic_sector_classification") 
+scaled_na_consumption <- read_csv(here::here("data", "raw", "scaled_na_consumption .csv")) 
 
 
-# 03 Calculate State Consumption from Leontief Matrix, final demand and plastic intensity ( f * L / m) in metric tons -----------------------------
 
-consum_2012_2020_total <- calc_state_consum(state_abbr = "CA", deflated_plastic_intensity = m,
-                              consumption_element = "Consumption_Complete") # only need long format? go back to summarise 
+calc_consum_bau <- function(bea_to_plastic, state_abbr, consumption_element, scaled_na_consumption, n_iterations) {
 
-# 04 Forecast (do first) -------------------------------------------------------------
-# load in data; slope of change in plastic intensity from model data 2012-2020 to get change in plastic intensity 
+  # 00 Sector percentages pre-processing ---------------------------------------
+  props <- preprocess_sectors(bea_to_plastic=bea_to_plastic)
+  
+  # 01 Create State Long Data ---------------------------------------------
+  download_rds_state_model(state_abbr = state_abbr) # Reactive input in shiny 
 
-forecast_consum <- calc_forecast(consum_2012_2020_total)
+  # 02 Calculate deflated/inflated plastic intensity (m) ------------------------
 
-# 05 Hindcast consumption  ------------------------------------------------
-
-consum_1950_2050 <- calc_hindcast(forecast_consum)
-
-# 06 Calculate A matrix power series   -------------------------------------------------------
-
-power_series <- calc_power_series("CA", n_iterations = 4) 
-
-# 07 Calculate A consumption in million metric tons of plastic 
-
-a_consum <-calc_a_consum("CA", power_series, m) 
+  m <- calc_deflated_plastic_int(state_abbr = state_abbr) 
 
 
-# 08 Sectors by A matrix consumption for 2012 to 2020 ---------------------
+  # 03 Calculate State Consumption from Leontief Matrix, final demand and plastic intensity ( f * L / m) in metric tons -----------------------------
 
-a_sector_consum <- calc_a_sector_consum(a_consum, props) # NUMBERS DO NOT MATCH SPREADSHEET HERE, props does not contain proportions for every BEA sector 
+  consum_2012_2020_total <- calc_state_consum(state_abbr = state_abbr, deflated_plastic_intensity = m,
+                                              consumption_element = consumption_element) # only need long format? go back to summarise 
 
-# 09 average out across time to apply to consum_1950_2050
+  # 04 Forecast (do first) -------------------------------------------------------------
+  # load in data; slope of change in plastic intensity from model data 2012-2020 to get change in plastic intensity 
 
-avg_props <- calc_props_avg(a_sector_consum)
+  forecast_consum <- calc_forecast(consum_2012_2020_total, scaled_na_consumption=scaled_na_consumption)
+
+  # 05 Hindcast consumption  ------------------------------------------------
+
+  consum_1950_2050 <- calc_hindcast(forecast_consum)
+
+  # 06 Calculate A matrix power series   -------------------------------------------------------
+
+  power_series <- calc_power_series(state_abbr=state_abbr, n_iterations = n_iterations) 
+
+  # 07 Calculate A consumption in million metric tons of plastic -------------------------------------------------------
+
+  a_consum <-calc_a_consum(state_abbr=state_abbr, power_series, m) 
 
 
-# 10 create consum_bau final data frame by sector --------------------------------
+  # 08 average proportion of consumption by plastic sector ---------------------
 
-consum_bau <- calc_final_bau_consum(avg_props, consum_1950_2050)
+  avg_props <- calc_plastic_sector_props(a_consum, props) 
+
+
+  # 09 create consum_bau final data frame by sector --------------------------------
+
+  consum_bau <- calc_final_bau_consum(avg_props, consum_1950_2050)
 
 return(consum_bau)
 
 }
+
+## EXAMPLE: testing the function
+consum_bau <- calc_consum_bau(bea_to_plastic=bea_to_plastic,
+                              state_abbr='CA',
+                              consumption_element = "Consumption_Complete",
+                              scaled_na_consumption = scaled_na_consumption,
+                              n_iterations=4)
 
 
