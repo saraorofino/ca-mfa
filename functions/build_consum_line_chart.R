0b4504232a6e5349b4b6f6620372fd3a0edf20a9
matthewroco-calvo <matthewf.rc@gmail.com>
2026-08-20 02:26:02 -0700

    added the grey color to line chart for BAU. added mutate(year = as.numeric.. to ensure reading in a numeric value rather than string
    added consum_line_charts to sr, sb54, and combined in the server and ui!

--- functions/build_consum_line_chart.R
+++ functions/build_consum_line_chart.R
@@ -12,9 +12,11 @@ build_consum_line_chart <- function(consum_bau,
   
   plot_data <- consum_bau |>
     filter(sector == "all_sec") |>
+    mutate(year = as.numeric(year)) |> 
     left_join(
       scenario_data |>
         filter(sector == "all_sec") |>
+        mutate(year = as.numeric(year)) |> 
         select(year, mt_plastic_sr),
       by = "year"
     )
@@ -25,7 +27,7 @@ build_consum_line_chart <- function(consum_bau,
       fill = "#687E03",
       alpha = 0.2
     ) +
-    geom_line(aes(y = mt_plastic_bau)) +
+    geom_line(aes(y = mt_plastic_bau), color = "#A8A8A8") +
     geom_line(aes(y = mt_plastic_sr), color = "#687E03", linetype = "dashed") +
     geom_vline(xintercept = implement_year, linetype = "dotted") +
     annotate(
@@ -40,5 +42,5 @@ build_consum_line_chart <- function(consum_bau,
     labs(title = plot_title,
          x = "Year",
          y = "Plastic Consumed Per Year (Million Metric Tons)") +
-    theme_classic()
+    theme_classic(base_family = "Times New Roman")
 }
