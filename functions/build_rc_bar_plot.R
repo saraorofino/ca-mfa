efbebd86de5a2f6552f4d8328e3a976e7860a7b2
matthewroco-calvo <matthewf.rc@gmail.com>
2026-08-24 09:36:57 -0700

    created a function for an alternative RC comparison plot, with it being a bar chart instead of lollipop. increased line width, and relabelled both the bar and lollipop charts to say recycled content intervention and business as usual rather than short versions

--- functions/build_rc_bar_plot.R
+++ functions/build_rc_bar_plot.R
@@ -1,10 +1,12 @@
-#' @title Build Recycled Content Consumption Comparison Lollipop Plot
+#' @title Build Recycled Content Consumption Comparison Bar Chart
 #' @param consum_bau Dataframe output from consum_bau function
 #' @param avoid_prod_rc Dataframe output from run_policy_rc
 #' @param scenario_color The hex code color for the recycled content scenario
-#' @description Builds a lollipop chart which compares virgin production in BAU and RC scenarios
+#' @description Builds a horizontal bar chart which compares virgin production in BAU and RC scenarios
 
-build_rc_comparison_plot <- function(consum_bau, avoid_prod_rc, scenario_color) {
+
+
+build_rc_bar_plot <- function(consum_bau, avoid_prod_rc, scenario_color) {
   
   bau_total <- consum_bau |> 
     filter(sector == "all_sec") |> 
@@ -16,22 +18,14 @@ build_rc_comparison_plot <- function(consum_bau, avoid_prod_rc, scenario_color) 
     mt_plastic = c(bau_total, bau_total - avoid_prod_rc)
   ) |>
     mutate(
-      Scenario = factor(Scenario, levels = c("Recycled Content Intervention", "Business As Usual")),
-      y_num = as.numeric(Scenario) * 0.3 #adding a line spacing for visual
+      Scenario = factor(Scenario, levels = c("Recycled Content Intervention", "Business As Usual"))
     )
   
   fill_values <- setNames(c(scenario_color, "black"), c("Recycled Content Intervention", "Business As Usual")) #bau remains same across charts, change here if needed
   
-  ggplot(rc_data, aes(x = mt_plastic, y = y_num, color = Scenario, fill = Scenario)) +
-    geom_segment(aes(x = 0, xend = mt_plastic, y = y_num, yend = y_num), linewidth = 1) +
-    geom_point(shape = 21, size = 4, color = "black") +
+  ggplot(rc_data, aes(x = mt_plastic, y = Scenario, fill = Scenario)) +
+    geom_col(width = 0.3) +
     scale_fill_manual(values = fill_values) +
-    scale_color_manual(values = fill_values) +
-    scale_y_continuous(
-      breaks = rc_data$y_num,
-      labels = rc_data$Scenario,
-      limits = c(0, max(rc_data$y_num) + 0.3) #adjusting the line spacing
-    ) +
     labs(
       x = "Total Virgin Plastic Production (Mt)",
       y = NULL
@@ -40,4 +34,3 @@ build_rc_comparison_plot <- function(consum_bau, avoid_prod_rc, scenario_color) 
     theme(legend.position = "none") +
     guides(color = "none")
 }
-
