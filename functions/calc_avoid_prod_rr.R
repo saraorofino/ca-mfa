f2df8eebe6eaec602143e91477c159ac5704dd3f
emma-ras <emmarasmussen@ucsb.edu>
2026-08-24 17:44:45 -0700

    functioning avoid prod rr

--- functions/calc_avoid_prod_rr.R
+++ functions/calc_avoid_prod_rr.R
@@ -7,18 +7,18 @@
 #' @return A summarized data frame 'avoid_prod_total_rr' with the total, in-state and out-of-state avoided production from 1950 to 2050 based on recycling rate target. 
 
 
-calc_avoid_prod_rr<- function(recyc_output_rr, recyc_output_bau, displacement_rate = 0.8, summary = FALSE) {
- avoid_prod_rr <- recyc_output_bau |>
-   left_join(recyc_output_rr, by = c("year", "sector")) |>
-    mutate(mt_avoid_prod = (recyc_output_rr$mt_secondary_plastic_output- recyc_output_bau$mt_secondary_plastic_output)* displacement_rate) |>
-  filter(sector != "all_sec") |> 
+calc_avoid_prod_rr <- function(recyc_output_rr, recyc_output_bau, displacement_rate = 0.8, summary = FALSE) {
+  avoid_prod_rr <- recyc_output_bau |>
+    left_join(recyc_output_rr, by = c("year", "sector")) |>
+    mutate(mt_avoid_prod = (mt_secondary_plastic_output.y - mt_secondary_plastic_output.x) * displacement_rate) |>
+    filter(sector != "all_sec") |>
     select(year, sector, mt_avoid_prod)
   
   if (!summary) {
     return(avoid_prod_rr)
   }
   
-  avoid_prod_total_rr <- avoid_prod_rr |>
-   filter(sector != "all_sec") |># removes all sector totals per year
-    summarise(total = sum(mt_avoid_prod))
-} 
+  avoid_prod_rr |>
+    summarise(total = sum(mt_avoid_prod, na.rm = TRUE))
+}
+
