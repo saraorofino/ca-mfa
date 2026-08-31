19bafff784539f0fd73266d369ed20f2cc458a11
matthewroco-calvo <matthewf.rc@gmail.com>
2026-08-31 13:08:23 -0700

    adding on to calc_avoid_prod. added displacement via recycling.

--- functions/calc_avoid_prod.R
+++ functions/calc_avoid_prod.R
@@ -8,11 +8,34 @@
 #' @return If summary 'TRUE' a value 'avoid_prod_total' of 1950-2050 cumulative megatons of avoided plastic production across all sectors.  
 
 calc_avoid_prod<- function(consum_bau, consum_sr, summary = FALSE) {
-  avoid_prod <- consum_sr |>
-    left_join(consum_bau, by = c("year", "sector")) |>
-    mutate(mt_avoid_prod = mt_plastic_bau - mt_plastic_sr) |>
+  
+  ## step 1: avoided production purely from reduced consumption / sr
+  
+  avoid_prod_consum <- consum_scenario |>
+    left_join(consum_bau, by = c("year", "sector"), suffix = c("_scenario", "_bau")) |> #joining the 2 dataframes and adding suffix
+    mutate(mt_avoid_prod_consum = mt_plastic_bau - mt_plastic_sr) |> #will return 0
     filter(sector != "all_sec") |> 
+    select(year, sector, mt_avoid_prod_consum)
+  
+  
+  ## step 2: displacement via recycling
+  
+  avoid_prod_rr <- recyc_output_scenario |> 
+    left_join(recyc_output_bau, by = c("year", "sector"), suffix = c("_scenario", "_bau")) |> 
+    mutate(mt_avoid_prod_recyc = (mt_secondary_plastic_output_scenario - mt_secondary_plastic_output_bau) * displacement_rate) |>
+    filter(sector != "all_sec") |>
+    select(year, sector, mt_avoid_prod_recyc)
+  
+  ## step 3: combine them
+  
+  avoid_prod <- avoid_prod_consum |>
+    left_join(avoid_prod_recyc, by = c("year", "sector")) |>
+    mutate(mt_avoid_prod = mt_avoid_prod_consum + mt_avoid_prod_recyc) |>
     select(year, sector, mt_avoid_prod)
+    
+  
+  
+  
   
   if (!summary) {
     return(avoid_prod)
@@ -21,4 +44,8 @@ calc_avoid_prod<- function(consum_bau, consum_sr, summary = FALSE) {
   avoid_prod_total <- avoid_prod |>
     filter(sector != "all_sec") |># removes all sector totals per year
     summarise(total = sum(mt_avoid_prod))
+  
+  
+  
+  
 } 
