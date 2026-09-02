eeb1e70f5720c7f3a3ab8d565e81e49cee72fd04
matthewroco-calvo <matthewf.rc@gmail.com>
2026-09-02 15:20:52 -0700

    flipped axis (vertical aligns with fig 8 in report), reordered scenario (a should be first), removed redundant legend

--- functions/build_comparison_bar.R
+++ functions/build_comparison_bar.R
@@ -9,18 +9,18 @@ build_avoid_prod_comparison_bar <- function(avoid_prod_a, avoid_prod_b,
     mt_plastic = c(avoid_prod_a, avoid_prod_b)
   ) |>
     mutate(
-      Scenario = factor(Scenario, levels = c(scenario_b_name, scenario_a_name))
+      Scenario = factor(Scenario, levels = c(scenario_a_name, scenario_b_name))
     )
   
   fill_values <- setNames(c(scenario_a_color, scenario_b_color), c(scenario_a_name, scenario_b_name))
   
-  ggplot(avoid_prod_data, aes(x = mt_plastic, y = Scenario, fill = Scenario)) +
+  ggplot(avoid_prod_data, aes(x = Scenario, y = mt_plastic, fill = Scenario)) +
     geom_col(width = 0.5) +
     scale_fill_manual(values = fill_values) +
     labs(
-      x = "Total Avoided Virgin Plastic Production (Mt)",
-      y = NULL
+      x = NULL,
+      y = "Avoided Virgin Plastic Production (Mt)"
     ) +
     theme_classic(base_family = "Times New Roman", base_size = 20) +
-    theme(legend.position = "right")
+    theme(legend.position = "none")
 }
